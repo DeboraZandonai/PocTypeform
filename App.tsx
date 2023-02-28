2fc566249023823a5742a620b7b471a3b7777945
“Debora <debohzandonaii@gmail.com>
2023-02-28 20:24:14 -0300

    feat: Ajustado

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
-  const runFirst = `
-    window.document.cookie = "Cookie_1=teste"
-  `;
   return (
     <View style={styles.container}>
       <WebView
         style={styles.container}
-        sharedCookiesEnabled
-        thirdPartyCookiesEnabled
-        injectedJavaScriptBeforeContentLoaded={runFirst}
         source={{
-          uri: "https://poc-webview-one.vercel.app/",
+          uri: "https://b4tc6xmis3u.typeform.com/to/ydXSNZaq",
         }}
       />
     </View>
